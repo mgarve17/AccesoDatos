0c98b268981db430c9c157bcc5eadf10fd077805
yo <cuentamaya@gmail.com>
2026-09-24 16:57:34 +0200

    EJ 1 REPASO

---
+++ TEMA1/AC1_nuevo/src/main/java/com/mycompany/ac1_nuevo/AC1_nuevo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ac1_nuevo;
+
+/**
+ *
+ * @author daw2
+ */
+public class AC1_nuevo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ TEMA1/AC1_nuevo/src/main/java/com/mycompany/ac1_nuevo/Gestor.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ac1_nuevo;
+
+import java.nio.file.Path;
+import java.util.List;
+
+/**
+ *
+ * @author daw2
+ */
+public class Gestor {
+    
+    public static boolean comprobar(){
+    
+        return false;
+    }
+    
+    public static List<Path> obtenerFicheros(){
+    
+        return null;
+    }
+    
+}
